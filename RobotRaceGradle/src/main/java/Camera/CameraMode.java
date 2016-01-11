6a547f2ebaeb66de8ace52c3605c101c8b941746
Arjan Boschman <a.boschman@hotmail.com>
2016-01-11 14:22:03 +0100

    Put the farPlane minimum value back in.

--- RobotRaceGradle/src/main/java/Camera/CameraMode.java
+++ RobotRaceGradle/src/main/java/Camera/CameraMode.java
@@ -10,6 +10,22 @@ import robotrace.Vector;
 public class CameraMode {
 
     /**
+     * The far plane must have a minimum distance, or else nothing will be
+     * visible when zoomed all the way in.
+     */
+    private static final int MIN_FAR_PLANE_DIST = 500;
+
+    public static CameraMode interpolateMode(CameraMode modeFrom, CameraMode modeTo, double distance) {
+        return new CameraMode(
+                modeFrom.eye.scale(1d - distance).add(modeTo.eye.scale(distance)),
+                modeFrom.center.scale(1d - distance).add(modeTo.center.scale(distance)),
+                modeFrom.up.scale(1d - distance).add(modeTo.up.scale(distance)),
+                (float) ((modeFrom.fovAngle * (1d - distance)) + (modeTo.fovAngle * (distance))),
+                (float) ((modeFrom.planeNear * (1d - distance)) + (modeTo.planeNear * (distance))),
+                (float) ((modeFrom.planeFar * (1d - distance)) + (modeTo.planeFar * (distance)))
+        );
+    }
+    /**
      * The position of the camera.
      */
     private final Vector eye;
@@ -37,7 +53,7 @@ public class CameraMode {
             this.fovAngle = fovAngle;
         }
         this.planeNear = 0.1f * dist;
-        this.planeFar = 10f * dist;
+        this.planeFar = Math.max(MIN_FAR_PLANE_DIST, 10f * dist);
     }
 
     private CameraMode(Vector eye, Vector center, Vector up, float fovAngle, float planeNear, float planeFar) {
@@ -47,17 +63,6 @@ public class CameraMode {
         this.fovAngle = fovAngle;
         this.planeNear = planeNear;
         this.planeFar = planeFar;
-    }
-
-    public static CameraMode interpolateMode(CameraMode modeFrom, CameraMode modeTo, double distance) {
-        return new CameraMode(
-                modeFrom.eye.scale(1d - distance).add(modeTo.eye.scale(distance)),
-                modeFrom.center.scale(1d - distance).add(modeTo.center.scale(distance)),
-                modeFrom.up.scale(1d - distance).add(modeTo.up.scale(distance)),
-                (float) ((modeFrom.fovAngle * (1d - distance)) + (modeTo.fovAngle * (distance))),
-                (float) ((modeFrom.planeNear * (1d - distance)) + (modeTo.planeNear * (distance))),
-                (float) ((modeFrom.planeFar * (1d - distance)) + (modeTo.planeFar * (distance)))
-        );
     }
 
     public void setView(GL2 gl, GLU glu, GlobalState gs) {
